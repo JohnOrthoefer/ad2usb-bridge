6d47f69da3f89a266e793e08b30fed30955e263e
John Orthoefer <john@orthoefer.org>
2022-09-28 22:08:49 -0400

    updated json strings

--- cmd/ad2usb/types.go
+++ cmd/ad2usb/types.go
@@ -10,30 +10,30 @@ type StatusLog struct {
 }
 
 type StatusBits struct {
-   Ready           bool
-   ArmedAway       bool
-   ArmedHome       bool
-   BacklightOn     bool
-   ProgrammingMode bool
-   Beeps           byte
-   ZoneBypassed    bool
-   ACPower         bool
-   ChimeOn         bool
-   AlarmEvent      bool
-   AlarmSounding   bool
-   BatteryLow      bool
-   EntryDelayOff   bool
-   FireAlarm       bool
-   CheckZone       bool
-   PerimeterOnly   bool
-   SystemFault     bool
+   Ready           bool    `json:"ready"`
+   ArmedAway       bool    `json:"armed_away"`
+   ArmedHome       bool    `json:"armed_stay"`
+   BacklightOn     bool    `json:"backlight_on"`
+   ProgrammingMode bool    `json:"programming_mode"`
+   Beeps           byte    `json:"beeps"`
+   ZoneBypassed    bool    `json:"zone_bypassed"`
+   ACPower         bool    `json:"ac_power"`
+   ChimeOn         bool    `json:"chime_on"`
+   AlarmEvent      bool    `json:"alarm_event_occurred"`
+   AlarmSounding   bool    `json:"alarm_sounding"`
+   BatteryLow      bool    `json:"battery_low"`
+   EntryDelayOff   bool    `json:"entry_delay_off"`
+   FireAlarm       bool    `json:"fire_alarm"`
+   CheckZone       bool    `json:"exit_now"`
+   PerimeterOnly   bool    `json:"perimeter_only"`
+   SystemFault     bool    `json:"system_issue"`
 }
 
 type Status struct {
    Bits           StatusBits
    RawData        string
-   NumericCode    uint
-   Message        string
+   NumericCode    uint  `json:"numeric_message"`
+   Message        string   `json:"alpha_message"`
    Last           time.Time
    Count          uint
 }
